cb0c997466331b6984ba20124882fbb584849896
allanlimo02 <allan.limo@student.moringaschool.com>
2021-09-18 00:55:07 +0300

    Created index and layouts hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        }
 }
