f4c675f36962cf42edda8d8f2f8c807371357ac9
allanlimo02 <allan.limo@student.moringaschool.com>
2021-09-18 09:31:12 +0300

    Created animal class and endangered class

---
+++ src/main/java/Animals/Endangered.java
@@ -0,0 +1,29 @@
+package Animals;
+
+public class Endangered extends Animal{
+    private String age;
+    private String health;
+
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public Endangered(String age, String health,String name) {
+        super(name);
+        this.age = age;
+        this.health = health;
+    }
+}
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+
+import Animals.Endangered;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
